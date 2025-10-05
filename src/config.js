303e64841c96b3ed6eef0a2d9853cb309670e19d
Tiago Navarro <navarrotiago194@gmail.com>
2025-10-05 13:35:22 -0300

    add middleware to fixed error in profile page

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const SERVER_URL = "http://localhost:3001";
-const SERVER_URL = "https://server-fb-store.onrender.com";
+const SERVER_URL = "http://localhost:3001";
+// const SERVER_URL = "https://server-fb-store.onrender.com";
 
 export const envConfig = {
   SERVER_URL,
@@ -10,4 +10,5 @@ export const envConfig = {
   API_REFRESH_USER: `${SERVER_URL}/api/refresh`,
   API_COMPLETE_VERIFY: `${SERVER_URL}/api/complete-verify`,
   API_REQUEST_TOKEN: `${SERVER_URL}/api/request-token`,
+  API_REFRESH_TOKEN: `${SERVER_URL}/api/refresh-token`,
 };
