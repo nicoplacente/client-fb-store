3ee8432632218d9301ecf843d1d7fa38257cfb2f
Tiago Navarro <navarrotiago194@gmail.com>
2025-10-22 10:48:19 -0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -17,56 +17,56 @@ export async function middleware(request) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
 
-  if (!accessToken && !refreshToken) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // if (!accessToken && !refreshToken) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
-  const isAccessValid = accessToken ? await verifyJWT(accessToken) : null;
+  // const isAccessValid = accessToken ? await verifyJWT(accessToken) : null;
 
-  if (isAccessValid) {
-    return NextResponse.next();
-  }
+  // if (isAccessValid) {
+  //   return NextResponse.next();
+  // }
 
-  if (refreshToken) {
-    const isRefreshValid = await verifyJWT(refreshToken);
-    if (isRefreshValid) {
-      try {
-        const res = await fetch(`${envConfig.API_REFRESH_TOKEN}`, {
-          method: "POST",
-          credentials: "include",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ refreshToken }),
-        });
+  // if (refreshToken) {
+  //   const isRefreshValid = await verifyJWT(refreshToken);
+  //   if (isRefreshValid) {
+  //     try {
+  //       const res = await fetch(`${envConfig.API_REFRESH_TOKEN}`, {
+  //         method: "POST",
+  //         credentials: "include",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify({ refreshToken }),
+  //       });
 
-        if (res.ok) {
-          const data = await res.json();
+  //       if (res.ok) {
+  //         const data = await res.json();
 
-          const response = NextResponse.next();
-          response.cookies.set("accessToken", data.accessToken, {
-            httpOnly: true,
-            secure: isProd,
-            sameSite: isProd ? "none" : "lax",
-            path: "/",
-          });
-          response.cookies.set("refreshToken", data.refreshToken, {
-            httpOnly: true,
-            secure: isProd,
-            sameSite: isProd ? "none" : "lax",
-            path: "/",
-          });
+  //         const response = NextResponse.next();
+  //         response.cookies.set("accessToken", data.accessToken, {
+  //           httpOnly: true,
+  //           secure: isProd,
+  //           sameSite: isProd ? "none" : "lax",
+  //           path: "/",
+  //         });
+  //         response.cookies.set("refreshToken", data.refreshToken, {
+  //           httpOnly: true,
+  //           secure: isProd,
+  //           sameSite: isProd ? "none" : "lax",
+  //           path: "/",
+  //         });
 
-          return response;
-        }
-      } catch (err) {
-        console.error("Error al refrescar token:", err);
-      }
-    }
-  }
+  //         return response;
+  //       }
+  //     } catch (err) {
+  //       console.error("Error al refrescar token:", err);
+  //     }
+  //   }
+  // }
 
-  const response = NextResponse.redirect(new URL("/", request.url));
-  response.cookies.delete("accessToken");
-  response.cookies.delete("refreshToken");
-  return response;
+  // const response = NextResponse.redirect(new URL("/", request.url));
+  // response.cookies.delete("accessToken");
+  // response.cookies.delete("refreshToken");
+  // return response;
 }
 
 export const config = {
